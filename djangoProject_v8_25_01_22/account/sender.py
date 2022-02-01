77acea77a9f1251ca3f433e38f49985ce64f8eea
Tomasz Strzeszewski <58040652+tomaszStr1020@users.noreply.github.com>
2022-02-01 22:14:27 +0100

    Add files via upload

---
+++ djangoProject_v8_25_01_22/account/sender.py
@@ -0,0 +1,18 @@
+import smtplib
+from email.message import EmailMessage
+import time
+from matplotlib import pyplot as plt
+def email_send(subject, body, to):
+    msg = EmailMessage()
+    msg.set_content(body)
+    msg['subject'] = subject
+    msg['to'] = to
+    user = 'noreplytrainapp@gmail.com'
+    msg['from'] = "TrainingApp"
+    password = 'PROJEKTprojekt##11'
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(user, password)
+    server.send_message(msg)
+    server.quit()
